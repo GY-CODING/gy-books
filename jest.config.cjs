8a017126e6f7e8baa38df735ec2046376beb785d
gy-gfigueras <willstar111yt@gmail.com>
2025-09-19 17:35:00 +0200

    fix: rename jest config to use cjs extension

---
+++ jest.config.cjs
@@ -0,0 +1,48 @@
+/* eslint-disable no-undef */
+/* ...existing code... */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/?(*.)+(test).[jt]s?(x)'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx,js,jsx}',
+    '!src/**/*.d.ts',
+    '!src/**/__mocks__/**',
+  ],
+  coverageReporters: ['html'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.nvm/',
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/.git/',
+  ],
+  modulePathIgnorePatterns: [
+    '<rootDir>/.nvm/',
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/.git/',
+  ],
+  moduleNameMapper: {
+    '^@/app/components/atoms/BookCardCompact(\\.(ts|tsx|js|jsx))?$':
+      '<rootDir>/__mocks__/BookCardCompact.js',
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^next/font/google$': '<rootDir>/__mocks__/nextFontGoogleMock.js',
+    '^next/image$': '<rootDir>/__mocks__/next-image-mock.js',
+    '^@auth0/nextjs-auth0$': '<rootDir>/__mocks__/auth0Mock.js',
+  },
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          jsx: 'react-jsx',
+        },
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: ['/node_modules/'],
+};
+/* ...existing code... */
